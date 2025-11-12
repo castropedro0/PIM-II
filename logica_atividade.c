6b53d0cd01361f24733b4e78bd8db4ea5a2d22f7
Guilherme Carvalho <guilhermecarvalho004@hotmail.com>
2025-11-12 18:16:45 -0300

    Refactor student identifier from CPF to email

--- logica_atividade.c
+++ logica_atividade.c
@@ -1,3 +1,5 @@
+// Arquivo: logica_atividades.c
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,16 +12,17 @@ void __attribute__((constructor)) init_locale_atividades() {
     setlocale(LC_ALL, "");
 }
 
-int buscar_nota(Nota *notas, int total, char *aluno_cpf, char *atividade_titulo) {
+// Busca por EMAIL do Aluno
+int buscar_nota(Nota *notas, int total, char *aluno_email, char *atividade_titulo) {
     for (int i = 0; i < total; i++) {
-        if (strcmp(notas[i].aluno_cpf, aluno_cpf) == 0 && strcmp(notas[i].atividade_titulo, atividade_titulo) == 0) {
+        if (strcmp(notas[i].aluno_email, aluno_email) == 0 && strcmp(notas[i].atividade_titulo, atividade_titulo) == 0) {
             return i;
         }
     }
     return -1;
 }
 
-// --- FUNÇÕES DE NOTAS (LANÇAR E EDITAR SEPARADAS) ---
+// --- FUNÇÕES DE NOTAS ---
 
 void inserir_nota(Nota *notas, int *total_notas, Pessoa *usuario_logado) {
 
@@ -36,13 +39,13 @@ void inserir_nota(Nota *notas, int *total_notas, Pessoa *usuario_logado) {
     Nota nova_nota;
     printf("\n--- Lançar Nova Nota ---\n");
 
-    printf("CPF do Aluno que receberá a nota: ");
-    scanf("%s", nova_nota.aluno_cpf);
+    printf("E-mail do Aluno que receberá a nota: ");
+    scanf("%s", nova_nota.aluno_email);
 
     printf("Título da Atividade: ");
     scanf(" %[^\n]", nova_nota.atividade_titulo);
 
-    if (buscar_nota(notas, *total_notas, nova_nota.aluno_cpf, nova_nota.atividade_titulo) != -1) {
+    if (buscar_nota(notas, *total_notas, nova_nota.aluno_email, nova_nota.atividade_titulo) != -1) {
         printf("\n!!! ERRO: Já existe uma nota para esta atividade e aluno. Use a opção EDITAR. !!!\n");
         return;
     }
@@ -56,7 +59,7 @@ void inserir_nota(Nota *notas, int *total_notas, Pessoa *usuario_logado) {
 
     notas[*total_notas] = nova_nota;
     (*total_notas)++;
-    printf("\nNota %.2f lançada com sucesso para o aluno %s.\n", nova_nota.valor, nova_nota.aluno_cpf);
+    printf("\nNota %.2f lançada com sucesso para o aluno %s.\n", nova_nota.valor, nova_nota.aluno_email);
 }
 
 void editar_nota(Nota *notas, int total_notas, Pessoa *usuario_logado) {
@@ -66,17 +69,17 @@ void editar_nota(Nota *notas, int total_notas, Pessoa *usuario_logado) {
         return;
     }
 
-    char aluno_cpf_busca[15];
+    char aluno_email_busca[TAM_EMAIL];
     char atividade_titulo_busca[100];
     float nova_nota;
 
     printf("\n--- Editar/Alterar Nota Existente ---\n");
-    printf("CPF do Aluno: ");
-    scanf("%s", aluno_cpf_busca);
+    printf("E-mail do Aluno: ");
+    scanf("%s", aluno_email_busca);
     printf("Título da Atividade: ");
     scanf(" %[^\n]", atividade_titulo_busca);
 
-    int index = buscar_nota(notas, total_notas, aluno_cpf_busca, atividade_titulo_busca);
+    int index = buscar_nota(notas, total_notas, aluno_email_busca, atividade_titulo_busca);
 
     if (index != -1) {
         printf("Nota atual para Atividade '%s': %.2f\n", atividade_titulo_busca, notas[index].valor);
@@ -99,19 +102,19 @@ void editar_nota(Nota *notas, int total_notas, Pessoa *usuario_logado) {
 void consultar_notas(Nota *notas, int total_notas, Pessoa *usuario_logado) {
     printf("\n--- Consulta de Notas ---\n");
 
-    char cpf_busca[15];
+    char email_busca[TAM_EMAIL];
     int notas_encontradas = 0;
 
     if (strcmp(usuario_logado->role, ROLE_ALUNO) == 0) {
-        strcpy(cpf_busca, usuario_logado->cpf);
+        strcpy(email_busca, usuario_logado->email);
         printf("Consultando suas notas...\n");
     } else {
-        printf("Digite o CPF do aluno para consulta: ");
-        scanf("%s", cpf_busca);
+        printf("Digite o E-mail do aluno para consulta: ");
+        scanf("%s", email_busca);
     }
 
     for (int i = 0; i < total_notas; i++) {
-        if (strcmp(notas[i].aluno_cpf, cpf_busca) == 0) {
+        if (strcmp(notas[i].aluno_email, email_busca) == 0) {
             printf("----------------------------------------\n");
             printf("Atividade: %s\n", notas[i].atividade_titulo);
             printf("Nota: %.2f\n", notas[i].valor);
@@ -119,7 +122,7 @@ void consultar_notas(Nota *notas, int total_notas, Pessoa *usuario_logado) {
         }
     }
 
-    if (notas_encontradas == 0) { printf("Nenhuma nota encontrada para o CPF %s.\n", cpf_busca); }
+    if (notas_encontradas == 0) { printf("Nenhuma nota encontrada para o E-mail %s.\n", email_busca); }
     printf("----------------------------------------\n");
 }
 
@@ -130,15 +133,17 @@ void excluir_nota(Nota *notas, int *total_notas, Pessoa *usuario_logado) {
         return;
     }
 
-    char aluno_cpf_busca[15];
+    char aluno_email_busca[TAM_EMAIL];
     char atividade_titulo_busca[100];
     char confirmacao;
 
-    printf("\n--- Excluir Nota  ---\n");
-    printf("CPF do Aluno: "); scanf("%s", aluno_cpf_busca);
-    printf("Título da Atividade: "); scanf(" %[^\n]", atividade_titulo_busca);
+    printf("\n--- Excluir Nota ---\n");
+    printf("E-mail do Aluno: ");
+    scanf("%s", aluno_email_busca);
+    printf("Título da Atividade: ");
+    scanf(" %[^\n]", atividade_titulo_busca);
 
-    int index = buscar_nota(notas, *total_notas, aluno_cpf_busca, atividade_titulo_busca);
+    int index = buscar_nota(notas, *total_notas, aluno_email_busca, atividade_titulo_busca);
 
     if (index != -1) {
         printf("Nota encontrada (%.2f). Confirma exclusão? (S/N): ", notas[index].valor);
@@ -158,33 +163,36 @@ void excluir_nota(Nota *notas, int *total_notas, Pessoa *usuario_logado) {
     }
 }
 
-// --- FUNÇÕES DE ATIVIDADES E TURMAS ---
+// --- FUNÇÕES DE ATIVIDADES E TURMAS (Corrigido o erro de Linker) ---
 
-void inserir_atividade(Atividade *atividades, int *total, char *professor_cpf) {
-    if (*total >= MAX_ATIVIDADES) { printf("Limite de atividades atingido.\n"); return; }
+void inserir_atividade(Atividade *atividades, int *total_atividades, char *professor_email) {
+    if (*total_atividades >= MAX_ATIVIDADES) { printf("Limite de atividades atingido.\n"); return; }
 
     Atividade nova_atividade;
 
-    printf("\n--- Inserir Nova Atividade  ---\n");
-    printf("Título: "); scanf(" %[^\n]", nova_atividade.titulo);
-    printf("Descrição: "); scanf(" %[^\n]", nova_atividade.descricao);
-    printf("Data de Entrega (DD/MM/AAAA): "); scanf(" %s", nova_atividade.data_entrega);
+    printf("\n--- Inserir Nova Atividade ---\n");
+    printf("Título: ");
+    scanf(" %[^\n]", nova_atividade.titulo);
+    printf("Descrição: ");
+    scanf(" %[^\n]", nova_atividade.descricao);
+    printf("Data de Entrega (DD/MM/AAAA): ");
+    scanf(" %s", nova_atividade.data_entrega);
 
-    strcpy(nova_atividade.professor_cpf, professor_cpf);
+    strcpy(nova_atividade.professor_email, professor_email);
 
-    atividades[*total] = nova_atividade;
-    (*total)++;
+    atividades[*total_atividades] = nova_atividade;
+    (*total_atividades)++;
     printf("Atividade '%s' cadastrada com sucesso.\n", nova_atividade.titulo);
 }
 
-void consultar_atividades_aluno(Atividade *atividades, int total) {
-    if (total == 0) { printf("\nNenhuma atividade cadastrada no sistema.\n"); return; }
+void consultar_atividades_aluno(Atividade *atividades, int total_atividades) {
+    if (total_atividades == 0) { printf("\nNenhuma atividade cadastrada no sistema.\n"); return; }
 
-    printf("\n--- Consulta de Aulas e Atividades  ---\n");
-    for (int i = 0; i < total; i++) {
+    printf("\n--- Consulta de Aulas e Atividades ---\n");
+    for (int i = 0; i < total_atividades; i++) {
         printf("----------------------------------------\n");
         printf("Atividade: %s\n", atividades[i].titulo);
-        printf("Professor: %s\n", atividades[i].professor_cpf);
+        printf("Professor (E-mail): %s\n", atividades[i].professor_email);
         printf("Entrega: %s\n", atividades[i].data_entrega);
     }
     printf("----------------------------------------\n");
@@ -194,19 +202,22 @@ void inserir_turma(Turma *turmas, int *total_turmas) {
     if (*total_turmas >= MAX_TURMAS) { printf("Limite de turmas atingido.\n"); return; }
 
     Turma nova_turma;
-    char cpf_professor_temp[15];
+    char email_professor_temp[TAM_EMAIL];
 
-    printf("\n--- Inserir Nova Turma  ---\n");
-    printf("Nome/Código da Turma: "); scanf(" %[^\n]", nova_turma.nome);
+    printf("\n--- Inserir Nova Turma ---\n");
+    printf("Nome/Código da Turma: ");
+    scanf(" %[^\n]", nova_turma.nome);
 
-    printf("Nome do Professor Responsável (Pendente se vazio): "); scanf(" %s", cpf_professor_temp);
+    printf("E-mail do Professor Responsável (PENDENTE se vazio): ");
+    scanf(" %s", email_professor_temp);
 
-    if (strcmp(cpf_professor_temp, "PENDENTE") != 0 && strlen(cpf_professor_temp) > 0) {
-        strcpy(nova_turma.professor_cpf, cpf_professor_temp);
+    if (strcmp(email_professor_temp, "PENDENTE") != 0 && strlen(email_professor_temp) > 0) {
+        strcpy(nova_turma.professor_email, email_professor_temp);
+        // Gera um número aleatório de alunos entre 1 e 5 (exemplo)
         srand(time(NULL));
         nova_turma.total_alunos = (rand() % 5) + 1;
     } else {
-        strcpy(nova_turma.professor_cpf, "PENDENTE");
+        strcpy(nova_turma.professor_email, "PENDENTE");
         nova_turma.total_alunos = 0;
     }
 
@@ -224,13 +235,14 @@ void consultar_turma(Turma *turmas, int total_turmas, Pessoa *usuario_logado) {
     if (strcmp(usuario_logado->role, ROLE_ADM) == 0 || strcmp(usuario_logado->role, ROLE_PROFESSOR) == 0) {
         printf("Lista de Turmas (ADM/Professor):\n");
         for (int i = 0; i < total_turmas; i++) {
-            printf("[%d] Turma: %s | Prof.: %s | Alunos: %d\n",
-                i + 1, turmas[i].nome, turmas[i].professor_cpf, turmas[i].total_alunos);
+            printf("[%d] Turma: %s | Prof. (E-mail): %s | Alunos: %d\n",
+                i + 1, turmas[i].nome, turmas[i].professor_email, turmas[i].total_alunos);
         }
     } else {
+        // Aluno: Apenas mostra a primeira turma como exemplo de sua turma
         Turma *minha_turma = &turmas[0];
         printf("Sua Turma de Interesse: %s\n", minha_turma->nome);
-        printf("- Professor Responsável: %s\n", (strcmp(minha_turma->professor_cpf, "PENDENTE") == 0) ? "A definir" : minha_turma->professor_cpf);
+        printf("- Professor Responsável (E-mail): %s\n", (strcmp(minha_turma->professor_email, "PENDENTE") == 0) ? "A definir" : minha_turma->professor_email);
     }
     printf("----------------------------------------------------\n");
 }
@@ -242,7 +254,6 @@ void gerar_relatorio() {
     printf("----------------------------------------------------\n");
 }
 
-
 // --- FUNÇÕES DE PERSISTÊNCIA (FORMATO TEXTO) ---
 
 void salvar_dados_sistema(Pessoa *pessoas, int total_pessoas, Atividade *atividades, int total_atividades, Turma *turmas, int total_turmas, Nota *notas, int total_notas) {
@@ -252,38 +263,37 @@ void salvar_dados_sistema(Pessoa *pessoas, int total_pessoas, Atividade *ativida
         return;
     }
 
-    // --- 1. Pessoas ---
+    // --- 1. Pessoas (4 campos: Nome|Email|Senha|Role) ---
     fprintf(file, "PESSOAS %d\n", total_pessoas);
     for (int i = 0; i < total_pessoas; i++) {
-        // Formato: Nome|CPF|Senha|Idade|Telefone|Endereco|Role
-        fprintf(file, "%s|%s|%s|%d|%s|%s|%s\n",
-                pessoas[i].nome, pessoas[i].cpf, pessoas[i].senha, pessoas[i].idade,
-                pessoas[i].telefone, pessoas[i].endereco, pessoas[i].role);
+        // Formato: Nome|Email|Senha|Role
+        fprintf(file, "%s|%s|%s|%s\n",
+                pessoas[i].nome, pessoas[i].email, pessoas[i].senha, pessoas[i].role);
     }
 
-    // --- 2. Atividades ---
+    // --- 2. Atividades (Chave: Professor_Email) ---
     fprintf(file, "ATIVIDADES %d\n", total_atividades);
     for (int i = 0; i < total_atividades; i++) {
-        // Formato: Titulo|Descricao|Professor_CPF|Data_Entrega
+        // Formato: Titulo|Descricao|Professor_Email|Data_Entrega
         fprintf(file, "%s|%s|%s|%s\n",
-                atividades[i].titulo, atividades[i].descricao, atividades[i].professor_cpf,
+                atividades[i].titulo, atividades[i].descricao, atividades[i].professor_email,
                 atividades[i].data_entrega);
     }
 
-    // --- 3. Turmas ---
+    // --- 3. Turmas (Chave: Professor_Email) ---
     fprintf(file, "TURMAS %d\n", total_turmas);
     for (int i = 0; i < total_turmas; i++) {
-        // Formato: Nome|Professor_CPF|Total_Alunos
+        // Formato: Nome|Professor_Email|Total_Alunos
         fprintf(file, "%s|%s|%d\n",
-                turmas[i].nome, turmas[i].professor_cpf, turmas[i].total_alunos);
+                turmas[i].nome, turmas[i].professor_email, turmas[i].total_alunos);
     }
 
-    // --- 4. Notas ---
+    // --- 4. Notas (Chave: Aluno_Email) ---
     fprintf(file, "NOTAS %d\n", total_notas);
     for (int i = 0; i < total_notas; i++) {
-        // Formato: Aluno_CPF|Atividade_Titulo|Valor
+        // Formato: Aluno_Email|Atividade_Titulo|Valor
         fprintf(file, "%s|%s|%.2f\n",
-                notas[i].aluno_cpf, notas[i].atividade_titulo, notas[i].valor);
+                notas[i].aluno_email, notas[i].atividade_titulo, notas[i].valor);
     }
 
     fclose(file);
@@ -298,59 +308,56 @@ void carregar_dados_sistema(Pessoa *pessoas, int *total_pessoas, Atividade *ativ
         return;
     }
 
-    char tipo_dado[20];
-    int i;
+    char header[20];
+    int count;
 
-    // --- 1. Pessoas ---
-    if (fscanf(file, "%s %d\n", tipo_dado, total_pessoas) == 2 && strcmp(tipo_dado, "PESSOAS") == 0) {
-        for (i = 0; i < *total_pessoas; i++) {
-            if (fscanf(file, "%[^|]|%[^|]|%[^|]|%d|%[^|]|%[^|]|%[^\n]\n",
-                       pessoas[i].nome, pessoas[i].cpf, pessoas[i].senha, &pessoas[i].idade,
-                       pessoas[i].telefone, pessoas[i].endereco, pessoas[i].role) != 7) {
-                printf("\n[ERRO DE LEITURA] Falha ao ler Pessoas na linha %d. Interrompendo.\n", i + 1);
-                *total_pessoas = i;
-                break;
-            }
-        }
-    }
-
-    // --- 2. Atividades ---
-    if (fscanf(file, "%s %d\n", tipo_dado, total_atividades) == 2 && strcmp(tipo_dado, "ATIVIDADES") == 0) {
-        for (i = 0; i < *total_atividades; i++) {
+    // 1. Pessoas (Novo formato com 4 campos: Nome|Email|Senha|Role)
+    if (fscanf(file, "%s %d\n", header, &count) == 2 && strcmp(header, "PESSOAS") == 0) {
+        *total_pessoas = 0;
+        for (int i = 0; i < count && i < MAX_PESSOAS; i++) {
             if (fscanf(file, "%[^|]|%[^|]|%[^|]|%[^\n]\n",
-                       atividades[i].titulo, atividades[i].descricao, atividades[i].professor_cpf,
-                       atividades[i].data_entrega) != 4) {
-                printf("\n[ERRO DE LEITURA] Falha ao ler Atividades na linha %d. Interrompendo.\n", i + 1);
-                *total_atividades = i;
+                pessoas[i].nome, pessoas[i].email, pessoas[i].senha, pessoas[i].role) == 4) {
+                (*total_pessoas)++;
+            } else {
+                printf("\n[PERSISTÊNCIA] Erro ao ler registro de Pessoa. Interrompendo carregamento.\n");
                 break;
             }
         }
     }
 
-    // --- 3. Turmas ---
-    if (fscanf(file, "%s %d\n", tipo_dado, total_turmas) == 2 && strcmp(tipo_dado, "TURMAS") == 0) {
-        for (i = 0; i < *total_turmas; i++) {
+    // 2. Atividades
+    if (fscanf(file, "%s %d\n", header, &count) == 2 && strcmp(header, "ATIVIDADES") == 0) {
+        *total_atividades = 0;
+        for (int i = 0; i < count && i < MAX_ATIVIDADES; i++) {
+            if (fscanf(file, "%[^|]|%[^|]|%[^|]|%[^\n]\n",
+                atividades[i].titulo, atividades[i].descricao, atividades[i].professor_email, atividades[i].data_entrega) == 4) {
+                (*total_atividades)++;
+            }
+        }
+    }
+
+    // 3. Turmas
+    if (fscanf(file, "%s %d\n", header, &count) == 2 && strcmp(header, "TURMAS") == 0) {
+        *total_turmas = 0;
+        for (int i = 0; i < count && i < MAX_TURMAS; i++) {
             if (fscanf(file, "%[^|]|%[^|]|%d\n",
-                       turmas[i].nome, turmas[i].professor_cpf, &turmas[i].total_alunos) != 3) {
-                printf("\n[ERRO DE LEITURA] Falha ao ler Turmas na linha %d. Interrompendo.\n", i + 1);
-                *total_turmas = i;
-                break;
+                turmas[i].nome, turmas[i].professor_email, &turmas[i].total_alunos) == 3) {
+                (*total_turmas)++;
             }
         }
     }
 
-    // --- 4. Notas ---
-    if (fscanf(file, "%s %d\n", tipo_dado, total_notas) == 2 && strcmp(tipo_dado, "NOTAS") == 0) {
-        for (i = 0; i < *total_notas; i++) {
+    // 4. Notas
+    if (fscanf(file, "%s %d\n", header, &count) == 2 && strcmp(header, "NOTAS") == 0) {
+        *total_notas = 0;
+        for (int i = 0; i < count && i < MAX_NOTAS; i++) {
             if (fscanf(file, "%[^|]|%[^|]|%f\n",
-                       notas[i].aluno_cpf, notas[i].atividade_titulo, &notas[i].valor) != 3) {
-                printf("\n[ERRO DE LEITURA] Falha ao ler Notas na linha %d. Interrompendo.\n", i + 1);
-                *total_notas = i;
-                break;
+                notas[i].aluno_email, notas[i].atividade_titulo, &notas[i].valor) == 3) {
+                (*total_notas)++;
             }
         }
     }
 
     fclose(file);
-    printf("\n Dados carregados do arquivo de texto.\n");
+    printf("\n[PERSISTÊNCIA] Dados carregados com sucesso. Total de %d usuários e %d notas.\n", *total_pessoas, *total_notas);
 }
